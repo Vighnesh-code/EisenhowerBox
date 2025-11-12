b9d077dd0d701a7b6ae5d54570f88da2caaa40e2
Vighnesh-code <vighneshzagade4@gmail.com>
2025-11-12 22:54:15 +0530

    Completed the grid structure

--- src/components/TaskContainer.jsx
+++ src/components/TaskContainer.jsx
@@ -2,21 +2,28 @@ import React from "react";
 
 const TaskContainer = () => {
   return (
-    <div className="bg-pink-200 h-full mx-50 border-7 border-[#53908a] mb-4 flex justify-center items-center py-30 px-40">
+    <div className="bg-[#7dccbe] h-full mx-50 border-7 border-[#53908a] mb-4 flex flex-col justify-center items-center pb-10 pt-10 px-40">
+      <div className="bg-white w-full h-15 mb-10">Input Field</div>
       <div className="grid grid-cols-[auto_1fr_1fr] grid-rows-[auto_1fr_1fr] size-full gap-2">
-        <div></div>
-        <div className="size-full border-4 border-foreground px-1/2">
-          <p>Urgent</p>
+        <div />
+        <div className="h-20 border-4 border-foreground flex justify-center items-center bg-white">
+          <p className="font-bold uppercase">Urgent</p>
         </div>
-        <div className="size-full border-4 border-foreground">Not Urgent</div>
-        <div className="size-full border-4 border-foreground">Important</div>
-        <div className="size-full border-4 border-foreground"></div>
-        <div className="size-full border-4 border-foreground"></div>
-        <div className="size-full border-4 border-foreground">
-          Not Important
+        <div className="size-full border-4 border-foreground flex justify-center items-center bg-white">
+          <p className="font-bold uppercase">Not Urgent</p>
         </div>
-        <div className="size-full border-4 border-foreground"></div>
-        <div className="size-full border-4 border-foreground"></div>
+        <div className="h-full w-20 border-4 border-foreground flex items-center justify-center bg-white">
+          <span className="-rotate-90 font-bold uppercase">Important</span>
+        </div>
+        <div className="size-full border-4 border-foreground bg-[#daf7ef]"></div>
+        <div className="size-full border-4 border-foreground bg-[#daf7ef]"></div>
+        <div className="h-full w-20 border-4 border-foreground flex items-center justify-center bg-white">
+          <span className="-rotate-90 font-bold uppercase whitespace-nowrap">
+            Not Important
+          </span>
+        </div>
+        <div className="size-full border-4 border-foreground bg-[#daf7ef]"></div>
+        <div className="size-full border-4 border-foreground bg-[#daf7ef]"></div>
       </div>
     </div>
   );
